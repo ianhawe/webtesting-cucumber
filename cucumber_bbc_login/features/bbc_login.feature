549ccc352de1f567e4e942ec56885bd91fadb0d5
Ian <ianhawe24@hotmail.co.uk>
2019-02-07 17:19:47 +0000

    scenario 3, password should only contain letters, and produce an error message based on the letter only message

--- cucumber_bbc_login/features/bbc_login.feature
+++ cucumber_bbc_login/features/bbc_login.feature
@@ -14,3 +14,10 @@ Feature: BBC Login
     When I try to login
     Then I recieve an error for having a short password
 
+  Scenario: Inputting incorrect short password and credentials shows an error
+    Given I access the bbc login page
+    And I input incorrect username details
+    And I input incorrect letter only password details
+    When I try to login
+    Then I recieve an error for having a short password
+
--- cucumber_bbc_login/features/lib/pages/bbc_signinpage.rb
+++ cucumber_bbc_login/features/lib/pages/bbc_signinpage.rb
@@ -8,14 +8,13 @@ class BbcSignIn_page
   USERNAME_ID = 'form-message-username'.freeze
   PASSWORD_ID = 'password-input'.freeze
   SUBMIT_ID = 'submit-button'.freeze
-  ERROR_MESSAGE_ID1 = 'form-message-username'.freeze
-  ERROR_MESSAGE_PW_ID = 'form-message-password'.freeze
-  ERROR_MESSAGE_ID2 = 'form-message-general'.freeze
+  ERROR_MESSAGE_USERNAME = 'form-message-username'.freeze
+  ERROR_MESSAGE_PASSWORD = 'form-message-password'.freeze
+  
 
   def initialize
     @error1 = 'Sorry, we can’t find an account with that email. You can register for a new account or get help here.'
     @error2 = 'Sorry, that password is too short. It needs to be eight characters or more.'
-    @error3 = `Sorry, those details don't match. Check you've typed them correctly.`
   end
   
   def name(name)
@@ -31,10 +30,10 @@ class BbcSignIn_page
   end
 
   def error_message1
-    find(:id, ERROR_MESSAGE_ID1).text
+    find(:id, ERROR_MESSAGE_USERNAME).text
   end
 
   def error_message2
-    find(:id, ERROR_MESSAGE_PW_ID).text
+    find(:id, ERROR_MESSAGE_PASSWORD).text
   end
 end
